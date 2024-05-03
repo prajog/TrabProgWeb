95afdca277b67312d52667f2298795a727653769
Gabi <gabrielapberetta@gmail.com>
2024-05-03 10:08:41 -0300

    first commit

---
+++ TRAB1 - WEB/js/logar.js
@@ -0,0 +1,25 @@
+var inID = document.getElementById("inID");
+var inSenha = document.getElementById("inSenha");
+var btEntrar = document.getElementById("btEntrar");
+
+function entrar() {
+    var id = inID.value;
+    var senha = inSenha.value;
+    
+    if (inID.value == "") {
+        alert("O campo 'CPF ou Cartão do SUS' não foi preenchido!");
+        inID.focus();
+    }else{
+        if(inSenha.value == "") {
+            alert("O campo 'Senha' não foi preenchido!");
+            inSenha.focus();
+        }else{
+            if (inID.value != "20231tiimi0332" || inSenha.value != "123456"){
+                alert("Usuário ou Senha foram digitados errado!")
+            }else{
+                window.location.href = "pagPrincipal.html";
+            }
+        }
+    }
+
+}btEntrar.addEventListener("click", entrar);
