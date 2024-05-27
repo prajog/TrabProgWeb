9e4fbf3331bac3a74c99496036ada5ecaac13bd9
Gabriela Prajo Beretta <101596018+prajog@users.noreply.github.com>
2024-05-27 12:56:39 -0300

    Alterações
    Todas as páginas possíveis

---
+++ TRAB1 - WEB/js/horarioAgenda.js
@@ -0,0 +1,32 @@
+const horarios = document.querySelector("#horarios");
+let selecionado = false;
+let elem = null;
+
+for (let i = 0; i< horarios.childElementCount; i++){
+    horarios.children[i].addEventListener("click", (ev) => {mudaCor(ev)});
+};
+
+// Funcao que muda a cor do elemento
+function mudaCor(ev){
+
+    // Se nenhum elemento foi selecionado...
+    if (selecionado === false){ 
+
+        // Liga a classe selHorario no elemento, mudando sua cor
+        ev.target.classList.toggle("selHorario");
+        selecionado = true;
+
+        // Pega e salva o elemento que chamou o evento click
+        elem = ev.target;
+    } else {
+
+        // Se o elemento salvo for o mesmo que o do click...
+        if (ev.target === elem){
+
+            // Muda a cor do elemento, para "deselecionar"
+            ev.target.classList.toggle("selHorario");
+
+            selecionado = false;
+        }
+    }
+}
