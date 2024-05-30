5965b42051fc50b42cabb8b8cd2c271339fc9c32
XpRook <137420775+XpRook@users.noreply.github.com>
2024-05-30 18:32:57 -0300

    Responsividade

--- TRAB1 - WEB/js/recuperar.js
+++ TRAB1 - WEB/js/recuperar.js
@@ -1,17 +1,17 @@
 var btRecuperar = document.getElementById("btRecuperar");
 var inEmail = document.getElementById("inEmail");
 
-function enviar() {
+function recuperar() {
   if(inEmail.value == ""){
     alert("O campo 'Email' não foi preenchido!");
     inEmail.focus();
   }else{
     window.location.href = "msgRcpEmail.html";
   }
-}btRecuperar.addEventListener("click", enviar);
+}btRecuperar.addEventListener("click", recuperar);
 
 document.addEventListener('keypress', function(event) {
     if (event.key == 'Enter') {
-      enviar();
+      recuperar();
     }
 });
